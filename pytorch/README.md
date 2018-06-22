1db03f9aa1f855b3f6bb2424be99558da574df95
whoami <whose@mail.ai>
2018-06-22 20:32:19 +0900

    Update: pytorch 0.2.0 -> 0.4.0

--- pytorch/README.md
+++ pytorch/README.md
@@ -3,15 +3,18 @@
 i followed the pytorch.org's "Get Started."
 
 ```bash
-conda install pytorch torchvision cuda80 -c soumith
+# Linux, conda, python3.6 cuda9.0
+conda install pytorch torchvision cuda90 -c pytorch
 ```
 
 and got
 
 ```bash
-torchvision               0.1.9            py36h7584368_1    soumith
-cuda80                    1.0                           0    soumith
-pytorch                   0.2.0           py36h53baedd_4cu80  [cuda80]  soumith
+cuda90:      1.0-h6433d27_0                      pytorch
+cudatoolkit: 9.0-h13b8566_0
+ninja:       1.8.2-py36h6bb024c_1
+pytorch:     0.4.0-py36_cuda9.0.176_cudnn7.1.2_1 pytorch [cuda90]
+torchvision: 0.2.1-py36_1                        pytorch
 ```
 
 ## Run
--- pytorch/linear.py
+++ pytorch/linear.py
@@ -11,11 +11,12 @@ import torch.optim as optim
 # http://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 
 # dataset
+batch_size = 30
 transform = transforms.ToTensor()
 set_train = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)  # [(1x28x28, 1)]
-loader_train = torch.utils.data.DataLoader(set_train, batch_size=4, shuffle=True, num_workers=2)
+loader_train = torch.utils.data.DataLoader(set_train, batch_size=batch_size, shuffle=True, num_workers=2)
 set_test = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-loader_test = torch.utils.data.DataLoader(set_test, batch_size=4, shuffle=True, num_workers=2)
+loader_test = torch.utils.data.DataLoader(set_test, batch_size=batch_size, shuffle=False, num_workers=2)
 
 
 # model
@@ -28,7 +29,7 @@ class Net(nn.Module):
 
     def forward(self, x):
         x = x.view(-1, 28 * 28)
-        y = F.softmax(self.lin(x))
+        y = F.log_softmax(self.lin(x), dim=-1)
         return y
 
 
@@ -42,25 +43,23 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 # training
-EPOCHS = 11
-VIEW_INTERVAL = 2000
+EPOCHS = 10
+VIEW_INTERVAL = 200
 for epoch in range(EPOCHS):
     running_loss = 0
-    for i, data in enumerate(loader_train):
-        x, y = data
+    for i, (x, y) in enumerate(loader_train):
         if torch.cuda.is_available():
             x, y = x.cuda(), y.cuda()
-        X, Y = Variable(x), Variable(y)
         optimizer.zero_grad()
-        Y_pred = model(X)
-        loss = criterion(Y_pred, Y)
+        y_pred = model(x)
+        loss = criterion(y_pred, y)
         loss.backward()
         optimizer.step()
 
         # report loss
-        running_loss += loss.data[0]
+        running_loss += loss.item()
         if i % VIEW_INTERVAL == VIEW_INTERVAL - 1:
-            print("Epoch {}, iteration {}; loss: {:.3f}".format(epoch + 1, i + 1, running_loss / VIEW_INTERVAL))
+            print(f"Epoch {epoch+1}, iteration {i+1}; loss: {(running_loss / VIEW_INTERVAL):.3f}")
             running_loss = 0
 
             # testing
@@ -72,7 +71,6 @@ for epoch in range(EPOCHS):
                 y_pred = model(Variable(images))
                 _, labels_pred = torch.max(y_pred.data, 1)
                 c = (labels_pred == labels).squeeze()
-                for k in c:
-                    count_correct += k
-                    count_total += 1
-            print("  Test Acc: {}".format(count_correct / count_total))
+                count_correct += c.sum().item()
+                count_total += len(c)
+            print(f"  Test Acc: {100.0 * count_correct / count_total :.2f}%")
